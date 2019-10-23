3f97f1c8b7847de8e491ea02968c2df8668d9d9e
sdonich <sdonich@gmail.com>
2019-10-23 19:30:07 +0200

    Add AppRouter

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import { router } from './routes/loginRoutes';
+import { AppRouter } from './AppRouter';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-import { router as controllerRouter } from './controllers/decorators/controller';
 import './controllers/loginController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['lsf'] }));
+app.use(AppRouter.getInstance());
 app.use(router);
-app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
